917ffde559fdfc63927089bce96036f1599fdc9c
godefroiroussel <gofff110@gmail.com>
2018-03-17 15:21:34 +0100

    Add : Seeder and initCoreData

--- Mon Pk & Moi/AppDelegate.swift
+++ Mon Pk & Moi/AppDelegate.swift
@@ -17,8 +17,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.initCoreData()
+        
         return true
     }
+    
+    /// Initialize data if necessary
+    func initCoreData() {
+        
+        let medicamentDAO = MedicamentCoreDataDAO()
+        
+        //Initialisation of 'medicament'
+        do { let medicaments = try medicamentDAO.getAllMedicaments()
+            if medicaments.count == 0 {
+                let medicamentSeeder = MedicamentSeeder(context: CoreDataManager.context)
+                medicamentSeeder.seedDataStore()
+            }
+        } catch {}
+        
+        }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
