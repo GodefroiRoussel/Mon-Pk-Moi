5b4090e2281cdb7c1b3eaa6e71e4539ef4b54cf7
Romain THEVENON <romain.thevenon@igtp4-mac06.isim.intra>
2018-03-21 10:33:59 +0100

    ajout vue ordonnance

---
+++ Mon Pk & Moi/OrdonnanceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrdonnanceTableViewCell.swift
+//  Mon Pk & Moi
+//
+//  Created by Romain THEVENON on 21/03/2018.
+//  Copyright © 2018 romain. All rights reserved.
+//
+
+import UIKit
+
+class OrdonnanceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
