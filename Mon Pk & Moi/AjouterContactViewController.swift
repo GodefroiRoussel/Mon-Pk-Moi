1729de8369f2fcac86fc12fa215adfce0069fe42
romainthevenon <romain.thevenon@hotmail.com>
2018-03-28 22:28:07 +0200

    début ajout contact

--- Mon Pk & Moi/AjouterContactViewController.swift
+++ Mon Pk & Moi/AjouterContactViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class AjouterContactViewController: UIViewController {
+class AjouterContactViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var typeContactLabel: UITextField!
     @IBOutlet weak var nomField: UITextField!
     @IBOutlet weak var prenomField: UITextField!
     @IBOutlet weak var numeroField: UITextField!
     @IBOutlet weak var adresseField: UITextField!
     
+    var pickerView = UIPickerView()
+    
+    var typeContacts : [TypeContact] = []
+    
+    let factory: CoreDataDAOFactory = CoreDataDAOFactory.getInstance()
+    
+    var selectedTypeContact : TypeContact? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let typeContactDAO : TypeContactDAO = factory.getTypeContactDAO()
+        do {
+            let typeconts: [TypeContact] = try typeContactDAO.getAllTypeContacts()
+            for typecontact in typeconts {
+                typeContacts.append(typecontact)
+            }
+        } catch let error as NSError {
+            print("error")
+        }
+        
+        pickerView.delegate = self
+        
+        typeContactLabel.inputView = pickerView
+        typeContactLabel.textAlignment = .center
+        typeContactLabel.placeholder = "Choisir un type de contact"
 
         // Do any additional setup after loading the view.
     }
@@ -28,6 +53,24 @@ class AjouterContactViewController: UIViewController {
     
     @IBAction func cancelAction(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typeContacts.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return typeContacts[row].libelle
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedTypeContact = typeContacts[row]
+        typeContactLabel.text = selectedTypeContact!.libelle
+        typeContactLabel.resignFirstResponder()
     }
 
     /*
